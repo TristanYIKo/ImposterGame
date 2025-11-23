27550244bdd905cfdcfcf1cd6b435ae097e9f3b2
Tristan Ko <tkyiko@uwaterloo.ca>
2025-11-22 22:17:52 -0500

    v1.6

--- lib/data.ts
+++ lib/data.ts
@@ -1,19 +1,138 @@
 export const CATEGORIES = {
     Food: [
-        "Pizza", "Sushi", "Burger", "Tacos", "Pasta", "Ice Cream", "Steak", "Salad", "Pancakes", "Donut"
+        "Pizza", "Burger", "Fries", "Hot Dog", "Tacos", "Burrito", "Pasta",
+        "Noodles", "Sushi", "Fried Chicken", "Steak", "Salad", "Sandwich",
+        "Soup", "Rice", "Ramen", "Dumplings", "Ice Cream", "Cake", "Cookies",
+        "Donut", "Muffin", "Waffles", "Pancakes", "Cereal", "Chocolate",
+        "Chips", "Popcorn", "Apple", "Banana", "Orange", "Grapes",
+        "Watermelon", "Nachos", "Hot Wings", "Bagel", "Pretzel", "Yogurt",
+        "Smoothie", "Pie"
     ],
+
     Places: [
-        "School", "Hospital", "Beach", "Library", "Gym", "Airport", "Cinema", "Park", "Museum", "Zoo"
+        "School", "Hospital", "Beach", "Airport", "Gym", "Library", "Restaurant",
+        "Park", "Mall", "Movie Theater", "Supermarket", "Stadium", "Hotel",
+        "Zoo", "Police Station", "Fire Station", "Train Station", "Bus Stop",
+        "Bank", "Office", "Home", "Playground", "Church", "Museum", "University",
+        "City Hall", "Pharmacy", "Post Office", "Gas Station", "Hair Salon",
+        "Coffee Shop", "Bakery", "Amusement Park", "Swimming Pool", "Castle",
+        "Mountain", "Forest", "Desert", "Island", "Bridge"
     ],
+
     Animals: [
-        "Lion", "Elephant", "Penguin", "Giraffe", "Dolphin", "Tiger", "Kangaroo", "Panda", "Zebra", "Monkey"
+        "Dog", "Cat", "Lion", "Tiger", "Elephant", "Bear", "Monkey", "Horse",
+        "Cow", "Sheep", "Pig", "Chicken", "Duck", "Rabbit", "Giraffe", "Zebra",
+        "Kangaroo", "Panda", "Koala", "Fox", "Wolf", "Deer", "Dolphin", "Whale",
+        "Shark", "Penguin", "Eagle", "Owl", "Snake", "Frog", "Tortoise",
+        "Crocodile", "Mouse", "Squirrel", "Bee", "Butterfly", "Fish", "Hippo",
+        "Rhino", "Goat"
     ],
+
     Jobs: [
-        "Doctor", "Teacher", "Police", "Chef", "Pilot", "Artist", "Firefighter", "Nurse", "Lawyer", "Farmer"
+        "Doctor", "Teacher", "Police Officer", "Firefighter", "Chef", "Pilot",
+        "Nurse", "Lawyer", "Engineer", "Farmer", "Scientist", "Athlete", "Actor",
+        "Musician", "Singer", "Dancer", "Mechanic", "Electrician", "Plumber",
+        "Cashier", "Waiter", "Barista", "Receptionist", "Taxi Driver",
+        "Bus Driver", "Programmer", "Designer", "Photographer", "Journalist",
+        "Soldier", "Dentist", "Librarian", "Construction Worker", "Coach",
+        "Judge", "Shopkeeper", "Security Guard", "Youtuber", "Streamer", "Mayor"
     ],
+
     Objects: [
-        "Phone", "Laptop", "Chair", "Table", "Car", "Book", "Pen", "Watch", "Shoes", "Camera"
+        "Phone", "Laptop", "Tablet", "TV", "Remote", "Chair", "Table", "Desk",
+        "Bed", "Sofa", "Lamp", "Book", "Notebook", "Pen", "Pencil", "Backpack",
+        "Wallet", "Keys", "Headphones", "Camera", "Watch", "Glasses", "Bottle",
+        "Mug", "Umbrella", "Shoes", "Jacket", "Hat", "Toothbrush", "Towel",
+        "Mirror", "Clock", "Keyboard", "Mouse", "Game Controller", "Charger",
+        "Ball", "Bicycle", "Suitcase", "Calculator"
+    ],
+
+    // Big list; covers most cards in the game. Add any new ones you care about.
+    ClashRoyale: [
+        "Skeletons", "Archers", "Knight", "Bomber", "Goblins", "Spear Goblins",
+        "Minions", "Bats", "Fire Spirits", "Ice Spirit", "Electro Spirit",
+        "Elite Barbarians", "Barbarians", "Rascals", "Royal Recruits",
+        "Skeleton Army", "Guards", "Goblin Gang", "Wall Breakers",
+        "Goblin Barrel", "Skeleton Barrel", "Skeleton Dragons",
+        "Baby Dragon", "Inferno Dragon", "Electro Dragon", "Phoenix",
+        "Giant", "Hog Rider", "Golem", "P.E.K.K.A", "Mini P.E.K.K.A",
+        "Mega Knight", "Mega Minion", "Lava Hound", "Goblin Giant",
+        "Royal Giant", "Giant Skeleton", "Balloon", "Cannon Cart", "Bowler",
+        "Hunter", "Executioner", "Dark Prince", "Prince", "Valkyrie",
+        "Witch", "Night Witch", "Wizard", "Ice Wizard", "Electro Wizard",
+        "Magic Archer", "Musketeer", "Dart Goblin", "Fisherman",
+        "Bandit", "Royal Ghost", "Lumberjack", "Ram Rider", "Monk",
+        "Golden Knight", "Skeleton King", "Mighty Miner", "Archer Queen",
+        "Princess", "Miner", "Graveyard", "Sparky",
+        "Cannon", "Bomb Tower", "Tesla", "Inferno Tower", "Mortar", "X-Bow",
+        "Goblin Cage", "Goblin Drill", "Goblin Hut", "Barbarian Hut",
+        "Furnace", "Tombstone", "Elixir Collector",
+        "Arrows", "Zap", "Snowball", "Royal Delivery", "Fireball", "Poison",
+        "Rocket", "Lightning", "Earthquake", "Freeze", "Rage", "Clone",
+        "Tornado", "The Log"
+    ],
+
+    Celebrities: [
+        "Taylor Swift", "Drake", "Rihanna", "Beyoncé", "Ariana Grande",
+        "Justin Bieber", "The Weeknd", "Billie Eilish", "Ed Sheeran",
+        "Bruno Mars", "Selena Gomez", "Katy Perry", "Lady Gaga", "Dua Lipa",
+        "Travis Scott", "Kanye West", "Kendrick Lamar", "Bad Bunny",
+        "Shakira", "Zendaya", "Tom Holland", "Dwayne Johnson",
+        "Ryan Reynolds", "Leonardo DiCaprio", "Robert Downey Jr.",
+        "Chris Hemsworth", "Chris Evans", "Scarlett Johansson",
+        "Margot Robbie", "Will Smith", "Kevin Hart", "Jim Carrey",
+        "Emma Stone", "Jennifer Lawrence", "Morgan Freeman", "Keanu Reeves",
+        "Cristiano Ronaldo", "Lionel Messi", "LeBron James", "Serena Williams"
+    ],
+
+    VideoGames: [
+        "Minecraft", "Fortnite", "Roblox", "GTA V", "Red Dead Redemption 2",
+        "League of Legends", "Valorant", "Counter-Strike", "Overwatch",
+        "Apex Legends", "Call of Duty", "FIFA", "NBA 2K", "Rocket League",
+        "Among Us", "Fall Guys", "Clash Royale", "Clash of Clans",
+        "Brawl Stars", "Pokemon GO", "The Legend of Zelda", "Breath of the Wild",
+        "Tears of the Kingdom", "Super Mario Odyssey", "Animal Crossing",
+        "Stardew Valley", "Hollow Knight", "Cuphead", "Elden Ring",
+        "Dark Souls", "Cyberpunk 2077", "The Witcher 3", "God of War",
+        "Spider-Man", "Spider-Man: Miles Morales", "Terraria", "Halo",
+        "Battlefield", "The Sims"
+    ],
+
+    Movies: [
+        "Titanic", "Avatar", "Avatar: The Way of Water", "The Lion King",
+        "Frozen", "Frozen II", "Toy Story", "Finding Nemo", "Inside Out",
+        "Monsters Inc", "Shrek", "Despicable Me", "Minions", "Coco",
+        "The Incredibles", "Jurassic Park", "Jaws", "Star Wars",
+        "The Empire Strikes Back", "Avengers: Endgame",
+        "Avengers: Infinity War", "Iron Man", "Spider-Man: No Way Home",
+        "The Dark Knight", "Inception", "Interstellar",
+        "Harry Potter and the Sorcerer's Stone", "Harry Potter and the Goblet of Fire",
+        "The Hunger Games", "Barbie", "Oppenheimer", "Guardians of the Galaxy",
+        "Black Panther", "Top Gun: Maverick", "Joker", "The Batman",
+        "Forrest Gump", "The Matrix", "Pirates of the Caribbean",
+        "Fast and Furious"
+    ],
+
+    AnimeCharacters: [
+        "Goku", "Vegeta", "Naruto", "Sasuke", "Sakura", "Kakashi",
+        "Luffy", "Zoro", "Nami", "Sanji", "Tanjiro", "Nezuko",
+        "Zenitsu", "Inosuke", "Eren Yeager", "Mikasa", "Levi",
+        "Deku", "Bakugo", "All Might", "Gojo", "Itadori",
+        "Light Yagami", "L", "Gon", "Killua", "Hisoka", "Saitama",
+        "Genos", "Mob", "Pikachu", "Ash Ketchum", "Sailor Moon",
+        "Ichigo Kurosaki", "Natsu Dragneel", "Erza Scarlet",
+        "Edward Elric", "Alphonse Elric", "Spike Spiegel", "Yor Forger"
+    ],
+
+    HouseholdObjects: [
+        "Toothbrush", "Toothpaste", "Soap", "Shampoo", "Towel", "Pillow",
+        "Blanket", "Bed", "Sofa", "Desk", "Chair", "Table", "Lamp",
+        "Curtain", "Mirror", "Clock", "Trash Can", "Laundry Basket",
+        "Vacuum Cleaner", "Broom", "Mop", "Bucket", "Plate", "Bowl",
+        "Cup", "Spoon", "Fork", "Knife", "Pan", "Pot", "Fridge",
+        "Microwave", "Oven", "Stove", "Sink", "Shower", "Toilet",
+        "Hair Dryer", "Iron"
     ]
-};
+} as const;
 
 export type CategoryKey = keyof typeof CATEGORIES;
